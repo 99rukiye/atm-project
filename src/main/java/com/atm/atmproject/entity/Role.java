7319b35433980e3418a8e0cac737b92af9d4e6a4
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-07-29 15:28:20 +0300

    Role

---
+++ src/main/java/com/atm/atmproject/entity/Role.java
@@ -0,0 +1,19 @@
+package com.atm.atmproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name; // ROLE_ADMIN or ROLE_USER
+
+    // getter-setter
+}
