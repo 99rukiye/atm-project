10b76d64538b17b29fb5499ec57fb97354cc5768
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-07-29 04:40:52 +0300

    UserDetailsServiceImpl

--- src/main/java/com/atm/atmproject/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/atm/atmproject/service/impl/UserDetailsServiceImpl.java
@@ -1,9 +1,9 @@
 package com.atm.atmproject.service.impl;
-import org.springframework.security.authentication.LockedException;
 
 import com.atm.atmproject.entity.User;
 import com.atm.atmproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,10 +19,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
+        User user = userRepository.findByEmail(username) // ✅ BURADA DÜZENLEME
                 .orElseThrow(() -> new UsernameNotFoundException("Kullanıcı bulunamadı: " + username));
 
-        // Kullanıcı bloke olmuşsa giriş yapamasın
         if (user.isLocked()) {
             throw new LockedException("Hesabınız bloke olmuştur.");
         }
@@ -33,5 +32,4 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 new ArrayList<>()
         );
     }
-
 }
