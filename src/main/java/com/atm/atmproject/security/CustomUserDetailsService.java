db3c2e14c3ec4bc67119a45bd1a07fa304df2e19
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-02 02:07:07 +0300

    CustomUserDetailsService

--- src/main/java/com/atm/atmproject/security/CustomUserDetailsService.java
+++ src/main/java/com/atm/atmproject/security/CustomUserDetailsService.java
@@ -2,26 +2,26 @@ package com.atm.atmproject.security;
 
 import com.atm.atmproject.entity.User;
 import com.atm.atmproject.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    @Autowired
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Kullanıcı bulunamadı: " + email));
-        return new CustomUserDetails(user);
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRole().name())
+                .accountLocked(user.isLocked())
+                .build();
     }
 }
