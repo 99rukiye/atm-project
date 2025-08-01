b799d112b172a3657d9a8f0520edcb80d547f116
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-02 02:08:49 +0300

    LoginResponseDTO

---
+++ src/main/java/com/atm/atmproject/dto/LoginResponseDTO.java
@@ -0,0 +1,41 @@
+package com.atm.atmproject.dto;
+
+public class LoginResponseDTO {
+    private String fullName;
+    private String email;
+    private double balance;
+    private String token;
+
+    // Getter ve Setter'lar
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
