5b44fd023f84b8a1eed5492712913a2629419694
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-02 02:08:14 +0300

    JwtUtil

---
+++ src/main/java/com/atm/atmproject/security/JwtUtil.java
@@ -0,0 +1,41 @@
+package com.atm.atmproject.security;
+
+import com.atm.atmproject.entity.User;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET_KEY = "atm-secret-key";
+    private final long EXPIRATION_TIME = 86400000; // 1 gün
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .claim("fullName", user.getFullName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
