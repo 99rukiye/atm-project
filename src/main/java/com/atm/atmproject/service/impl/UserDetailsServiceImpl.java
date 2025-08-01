e8fb7e910f30f64f03d30abbd83f61dcbb12df20
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-02 02:02:35 +0300

    UserDetailsServiceImpl

--- src/main/java/com/atm/atmproject/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/atm/atmproject/service/impl/UserDetailsServiceImpl.java
@@ -19,7 +19,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username) // ✅ BURADA DÜZENLEME
+        User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Kullanıcı bulunamadı: " + username));
 
         if (user.isLocked()) {
