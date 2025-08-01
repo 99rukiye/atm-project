8f858a81a250471be8963344e4e236bafe3da00c
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-02 02:06:51 +0300

    CustomUserDetails

--- src/main/java/com/atm/atmproject/security/CustomUserDetails.java
+++ src/main/java/com/atm/atmproject/security/CustomUserDetails.java
@@ -28,26 +28,26 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getEmail(); // email ile login olacak
+        return user.getEmail();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true; // Süresi dolmamış hesap
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return !user.isLocked(); // kilitli değilse true döner
+        return !user.isLocked();
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true; // şifre süresi dolmamış
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return true; // aktif kullanıcı
+        return true;
     }
 }
